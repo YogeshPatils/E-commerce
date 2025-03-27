2808d9841b8abeb7f2fb16155df4017691c293ad
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-03-27 13:37:41 +0530

    created home,product,category view for core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Category,Product,ProductVarient
 # Create your views here.
+class HomeView(View):
+    def get(self,request,*args,**kwargs):
+        qs=Category.objects.filter(parent_category=None)
+        
+        return render(request,'authentication/home.html',{'categorys':qs}) 
+    
+class CategoryView(View):
+    def get(self,request,*args,**kwargs):
+        obj=Category.objects.get(name=kwargs.get('category'))
+        product_data=Product.objects.filter(category_id=obj)
+        varient_data=ProductVarient.objects.filter(product__in=product_data)
+        data=zip(product_data,varient_data)
+        return render(request,'core/category.html',{'data':data})
+
+class ProductView(View):
+    def get(self,request,*args,**kwargs):
+        vd=ProductVarient.objects.get(varient_id=kwargs.get('varient_id'))
+        pd=Product.objects.get(name=vd.product)
+        return render(request,'core/productdetail.html',{'vd':vd,'pd':pd})
