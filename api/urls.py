7e231f34babf107d25aa0d73ba5a422386ac9f9f
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-07 22:25:28 +0530

    url for product api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import userView,productView
+from .views import userView,productView,productVarientView
 urlpatterns=[
     path('userapi/',userView,name='userapi'),
+    path('productvarientapi/',productVarientView,name='productvarientapi'),
     path('productapi/',productView,name='productapi'),
 ]
