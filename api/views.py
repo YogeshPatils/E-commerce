51bc271c28d902aa2889eda1ca548bc75d005da5
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-07 22:28:59 +0530

    added code snippet for post request

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer,ProductSerializer
+from .serializers import UserSerializer,ProductSerializer,ProductVarientSerializer
 from django.contrib.auth.models import User
-from core.models import ProductVarient
+from core.models import ProductVarient,Product
 @api_view(['GET'])
 def userView(request):
     data=User.objects.all()
@@ -11,9 +11,29 @@ def userView(request):
 
 @api_view(['GET','POST'])
 def productView(request):
-    data=ProductVarient.objects.all()
-    serializer=ProductSerializer(data,many=True)
-    return Response(serializer.data)
+    if request.method=='GET':
+        data=Product.objects.all()
+        serializer=ProductSerializer(data,many=True)
+        return Response(serializer.data)
+    if request.method=='POST':
+        serializer=ProductSerializer(data=request.data,many=isinstance(request.data,list))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+
+@api_view(['GET','POST'])
+def productVarientView(request):
+    if request.method=='GET':
+        data=ProductVarient.objects.all()
+        serializer=ProductVarientSerializer(data,many=True)
+        return Response(serializer.data)
+    if request.method=='POST':
+        serializer=ProductVarientSerializer(data=request.data,many=isinstance(request.data,list))
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
 
 
     
