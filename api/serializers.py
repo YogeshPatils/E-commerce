790ef90e8e3ad4d699537884c7972d34943e1f75
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-07 22:24:18 +0530

    created product varient serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from core.models import ProductVarient
+from core.models import ProductVarient,Product
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields = ['id', 'username','first_name','last_name','email']
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductVarientSerializer(serializers.ModelSerializer):
     class Meta:
         model=ProductVarient
         fields='__all__'
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields='__all__'
