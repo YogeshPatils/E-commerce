a6e3b6b82c5aef3ccd7420491e849fd1b890082f
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-08 13:09:19 +0530

    registered models to admin

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Order,OrderItem
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['order_id','user_id','total','status','created_at']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display=['order_item_id','order_id','product_id','quantity','price']
