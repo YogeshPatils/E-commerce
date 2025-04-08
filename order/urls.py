558649b08c91868ca1a0ea8c1ae40b385618d0ca
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-08 07:49:45 +0530

    created a addtocart url

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import AddToCartView
 
 urlpatterns=[
-
+    path('addtocart/<slug:slug/>',AddToCartView.as_view(),name='addtocart')
 ]
