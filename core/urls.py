4d6debeb1bcca9484bd09291faf3c28e9dd62dc1
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-09 08:06:41 +0530

    created url to the search view class

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView,CategoryView,ProductView
+from .views import HomeView,CategoryView,ProductView,SearchView
 urlpatterns=[
     path('home/',HomeView.as_view(),name='home'),
     path('category/<str:category>/',CategoryView.as_view(),name='category'),
     path('product/<str:varient_id>/',ProductView.as_view(),name='product'),
+    path('search/',SearchView.as_view(),name='search')
 
 ]
