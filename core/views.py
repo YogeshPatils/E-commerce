af1fd8d73e9fcd2693e14913d97da7b82592ba0c
Yogesh Patil <yogeshdpatils3@gmail.com>
2025-04-09 08:06:08 +0530

    created searchview class

--- core/views.py
+++ core/views.py
@@ -21,3 +21,15 @@ class ProductView(View):
         vd=ProductVarient.objects.get(varient_id=kwargs.get('varient_id'))
         pd=Product.objects.get(name=vd.product)
         return render(request,'core/productdetail.html',{'vd':vd,'pd':pd})
+
+class SearchView(View):
+    def get(self, request):
+        query = request.GET.get('q', '')
+        data = []
+
+        if query:
+            products = Product.objects.filter(name__icontains=query)
+            varient_map = {v.product.product_id: v for v in ProductVarient.objects.filter(product__in=products)}
+            data = [(pd, varient_map.get(pd.product_id)) for pd in products]
+
+        return render(request, 'core/search_results.html', {'data': data, 'query': query})
